05e7be6a637704d86d6e8ddbb98090231e3132b4
Lizhou <2576819374@qq.com>
2024-01-07 21:21:29 +0800

    1月7日，第二次提交，（CV大法）完成用户登录

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/SysUserService.java
@@ -0,0 +1,18 @@
+package com.atguigu.spzx.manager.service;
+
+import com.atguigu.spzx.model.dto.system.LoginDto;
+import com.atguigu.spzx.model.entity.system.SysUser;
+import com.atguigu.spzx.model.vo.system.LoginVo;
+
+public interface SysUserService {
+
+    /**
+     * 根据用户名查询用户数据
+     * @return
+     */
+    public abstract LoginVo login(LoginDto loginDto) ;
+    public SysUser getUserInfo(String token);
+
+    public void logout(String token);
+
+}
---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,91 @@
+package com.atguigu.spzx.manager.service.impl;
+
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSON;
+import com.atguigu.spzx.common.exception.GuiguException;
+import com.atguigu.spzx.manager.mapper.SysUserMapper;
+import com.atguigu.spzx.manager.service.SysUserService;
+import com.atguigu.spzx.model.dto.system.LoginDto;
+import com.atguigu.spzx.model.entity.system.SysUser;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.atguigu.spzx.model.vo.system.LoginVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class SysUserServiceImpl implements SysUserService {
+
+    @Autowired
+    private SysUserMapper sysUserMapper ;
+
+    @Autowired
+    private RedisTemplate<String , String> redisTemplate ;
+
+    @Override
+    public LoginVo login(LoginDto loginDto) {
+        // 校验验证码是否正确
+        String captcha = loginDto.getCaptcha();     // 用户输入的验证码
+        String codeKey = loginDto.getCodeKey();     // redis中验证码的数据key
+
+        System.out.println("captcha = " + captcha);
+        System.out.println("codeKey = " + codeKey);
+        // 从Redis中获取验证码
+        String redisCode = redisTemplate.opsForValue().get("user:login:validatecode:" + codeKey);
+        System.out.println("redisCode = " + redisCode);
+        if(StrUtil.isEmpty(redisCode) || !StrUtil.equalsIgnoreCase(redisCode , captcha)) {
+            throw new GuiguException(ResultCodeEnum.VALIDATECODE_ERROR) ;
+        }
+
+        // 验证通过删除redis中的验证码
+        redisTemplate.delete("user:login:validatecode:" + codeKey) ;
+
+
+        // 根据用户名查询用户
+        SysUser sysUser = sysUserMapper.selectByUserName(loginDto.getUserName());
+        if(sysUser == null) {
+            throw new GuiguException(ResultCodeEnum.LOGIN_ERROR);
+            //throw new RuntimeException("用户名或者密码错误") ;
+        }
+
+        // 验证密码是否正确
+        String inputPassword = loginDto.getPassword();
+        String md5InputPassword = DigestUtils.md5DigestAsHex(inputPassword.getBytes());
+        if(!md5InputPassword.equals(sysUser.getPassword())) {
+            throw new RuntimeException("用户名或者密码错误") ;
+        }
+//        System.out.println("登录成功");
+        // 生成令牌，保存数据到Redis中
+        String token = UUID.randomUUID().toString().replace("-", "");
+
+
+        redisTemplate.opsForValue().set("user:login:" + token , JSON.toJSONString(sysUser) , 30 , TimeUnit.MINUTES);
+
+        // 构建响应结果对象
+        LoginVo loginVo = new LoginVo() ;
+        loginVo.setToken(token);
+        loginVo.setRefresh_token("");
+        System.out.println("生成令牌，保存数据到Redis中 : token = " + token);
+
+        // 返回
+        return loginVo;
+    }
+
+    public SysUser getUserInfo(String token) {
+        String userJson = redisTemplate.opsForValue().get("user:login:" + token);
+        return JSON.parseObject(userJson , SysUser.class) ;
+    }
+
+    @Override
+    public void logout(String token) {
+        redisTemplate.delete("user:login:" + token) ;
+    }
+}
+
+
+
+
